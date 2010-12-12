7a8872d4ce5b37076ab47df88c91051565401104
bananow <bananow@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-12 20:57:58 +0000

    Prawie skończony operatorOX

---
+++ trunk/Algorytmy ewolucyjne flow shop/src/flowshop/Para.java
@@ -0,0 +1,45 @@
+package flowshop;
+
+/**
+ *
+ * @author Jakub Banaszewski
+ */
+public class Para<A, B> {
+
+    private A pierwszy;
+    private B drugi;
+
+    Para(A y, B x) {
+        pierwszy = y;
+        drugi = x;
+    }
+
+    public A getFirst() {
+        return pierwszy;
+    }
+
+    public B getSecond() {
+        return drugi;
+    }
+
+    @Override
+    public String toString() {
+        return "Para : \n pierszy : " + pierwszy.toString() + "\n drugi : " + drugi.toString() + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Para))
+            throw new ClassCastException("Nieprawidłowe porównanie Pary");
+        Para p = (Para) o;
+        return pierwszy.equals(p.getFirst()) && drugi.equals(p.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + (this.pierwszy != null ? this.pierwszy.hashCode() : 0);
+        hash = 79 * hash + (this.drugi != null ? this.drugi.hashCode() : 0);
+        return hash;
+    }
+}
--- trunk/Algorytmy ewolucyjne flow shop/src/flowshop/osobnikFlowShop.java
+++ trunk/Algorytmy ewolucyjne flow shop/src/flowshop/osobnikFlowShop.java
@@ -2,9 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package flowshop;
-
 
 import flowshop.Interfejsy.iOsobnik;
 import java.util.LinkedList;
@@ -15,73 +13,83 @@ import java.util.Random;
  *
  * @author Łukasz Synówka
  */
-public class osobnikFlowShop implements iOsobnik
-{
+public class osobnikFlowShop extends iOsobnik {
+
     int[] permutacja;
     int dlugoscPermutacji;
 
-    public osobnikFlowShop()
-    {
+    public osobnikFlowShop() {
         this.dlugoscPermutacji = 100;
         permutacja = new int[100];
-        List <Integer>pula = new LinkedList<Integer>();
-
-        for (int i=0;i<this.dlugoscPermutacji;i++)
-        {
-            pula.add(i);
-        }
-        Random r = new Random();
-        int a;
-        for (int i=this.dlugoscPermutacji;i>0;i--)
-        {
-            a = r.nextInt(i);
-            this.permutacja[i-1] = pula.remove(a);
-        }
-    }
-
-    public osobnikFlowShop(int dl)
-    {
-        permutacja = new int[dl];
-        this.dlugoscPermutacji=dl;
         List<Integer> pula = new LinkedList<Integer>();
 
-        for (int i=0;i<this.dlugoscPermutacji;i++)
-        {
+        for (int i = 0; i < this.dlugoscPermutacji; i++) {
             pula.add(i);
         }
         Random r = new Random();
         int a;
-        for (int i=this.dlugoscPermutacji;i>0;i--)
-        {
+        for (int i = this.dlugoscPermutacji; i > 0; i--) {
             a = r.nextInt(i);
-            this.permutacja[i-1] = pula.remove(a);
+            this.permutacja[i - 1] = pula.remove(a);
         }
     }
 
-    public osobnikFlowShop(int dl, int[] gen)
-    {
+    public osobnikFlowShop(int dl) {
+        permutacja = new int[dl];
+        this.dlugoscPermutacji = dl;
+        List<Integer> pula = new LinkedList<Integer>();
+
+        for (int i = 0; i < this.dlugoscPermutacji; i++) {
+            pula.add(i);
+        }
+        Random r = new Random();
+        int a;
+        for (int i = this.dlugoscPermutacji; i > 0; i--) {
+            a = r.nextInt(i);
+            this.permutacja[i - 1] = pula.remove(a);
+        }
+    }
+
+    public osobnikFlowShop(int dl, int[] gen) {
         this.dlugoscPermutacji = dl;
         this.permutacja = gen;
     }
 
-
-    public void modyfikujGen(int pozycja, Object wartosc)
-    {
+    public void modyfikujGen(int pozycja, Object wartosc) {
         int wartInt = (Integer) (wartosc);
-        if (pozycja<this.dlugoscPermutacji) this.permutacja[pozycja] = wartInt;
+        if (pozycja < this.dlugoscPermutacji) {
+            this.permutacja[pozycja] = wartInt;
+        }
     }
 
-    public Object wartoscOsobnika(int pozycja)
-    {
+    public Object wartoscOsobnika(int pozycja) {
         return this.permutacja[pozycja];
     }
 
-    public int dlugoscGenomu()
-    {
+    public int dlugoscGenomu() {
         return this.dlugoscPermutacji;
     }
 
+    @Override
+    protected Object clone() {
+        int[] newPerm = new int[dlugoscPermutacji];
+        System.arraycopy(permutacja, 0, newPerm, 0, dlugoscPermutacji);
+        return new osobnikFlowShop(dlugoscPermutacji, newPerm);
+    }
 
+    @Override
+    public int znajdzPozGenu(int zakresOd, int zakresDo, Object wartosc) {
+        for (int i = zakresOd;i<zakresDo;i++)
+            if (wartosc.equals(permutacja[i])) return i;
+        return zakresDo;
+    }
 
-
+    @Override
+    public int znajdzPozGenuPoza(int zakresOd, int zakresDo, Object wartosc) {
+        for (int i = 1;i<zakresOd;i++)
+            if (wartosc.equals(permutacja[i])) return i;
+        for (int i= zakresDo; i < dlugoscPermutacji; i++)
+            if (wartosc.equals(permutacja[i])) return i;
+        return dlugoscPermutacji;
+    }
 }
