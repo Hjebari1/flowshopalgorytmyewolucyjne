d58f8f850618fcd6d98b30ae9d5f6d0b7d0f2101
bananow <bananow@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-13 15:39:51 +0000

    Operatory przechodzą testy :)

--- trunk/Algorytmy ewolucyjne flow shop/src/flowshop/operatorOX.java
+++ trunk/Algorytmy ewolucyjne flow shop/src/flowshop/operatorOX.java
@@ -10,27 +10,30 @@ import java.util.Random;
  * Operator krzyżowania osobników OX (Order Crossover)
  */
 public class operatorOX extends iOperatorKrzyżowania {
-    
 
     public operatorOX(populacja daneWejsciowe) {
         zbiorOsobnikow = daneWejsciowe;
     }
 
-
     public void wykonaj() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-   public Para<iOsobnik, iOsobnik> krzyzuj(iOsobnik o1, iOsobnik o2,int poz1,int poz2) throws CloneNotSupportedException, Exception { //domyślnie private, dla testów public
-       if (o1.dlugoscGenomu() != o2.dlugoscGenomu()) throw new Exception("Nierówne genomy do krzyżowania!");
+    public Para<iOsobnik, iOsobnik> krzyzuj(iOsobnik o1, iOsobnik o2, int poz1, int poz2) throws CloneNotSupportedException, Exception { //domyślnie private, dla testów public
+        if (o1.dlugoscGenomu() != o2.dlugoscGenomu()) {
+            throw new Exception("Nierówne genomy do krzyżowania!");
+        }
         int size = o1.dlugoscGenomu();
-        if (size == 0) throw new Exception("Pusty genom do krzyżowania!");
+        if (size == 0) {
+            throw new Exception("Pusty genom do krzyżowania!");
+        }
 
         iOsobnik wyn1 = o1.makeCopy();
         iOsobnik wyn2 = o2.makeCopy();
-       
-        if (poz1 == poz2)
+
+        if (poz1 == poz2) {
             return new Para<iOsobnik, iOsobnik>(o1, o2);
+        }
         if (poz1 > poz2) {
             int tmp = poz1;
             poz1 = poz2;
@@ -55,39 +58,37 @@ public class operatorOX extends iOperatorKrzyżowania {
             wyn2.modyfikujGen(pozZmian, wyn1.wartoscOsobnika(i));
         }
         //faza OX
-        iOsobnik wyn4 = wyn1.makeCopy();
+        iOsobnik wyn4 = wyn1.makeCopy(); //trudna numeracja, nieintuicjyjna (jeśli jakaś intuicyjna istnieje)
         iOsobnik wyn3 = wyn2.makeCopy();
 
-        for (int i = 1; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             pozZmian = wyn2.znajdzPozGenu(0, size, wyn4.wartoscOsobnika(i));
             if (poz1 <= pozZmian && pozZmian < poz2) {
-                wyn4.modyfikujGen(i,null);
+                wyn4.modyfikujGen(i, iOsobnik.pusto);
             }
             pozZmian = wyn1.znajdzPozGenu(0, size, wyn3.wartoscOsobnika(i));
             if (poz1 <= pozZmian && pozZmian < poz2) {
-                wyn3.modyfikujGen(i,null);
+                wyn3.modyfikujGen(i, iOsobnik.pusto);
             }
-            
-            int j = poz2;
-            for (i = 0; i < size ; i ++)
-            {
-                if (wyn4.wartoscOsobnika((i + poz2) % size) == null) continue;
-                else {
-                    wyn1.modyfikujGen( j % size ,wyn4.wartoscOsobnika((i + poz2)% size));
-                    j++;
-                    }
+        }
+        int j = poz2;
+        for (int i = 0; i < size; i++) {
+            if (wyn4.wartoscOsobnika((i + poz2) % size) == iOsobnik.pusto) {
+                continue;
+            } else {
+                wyn2.modyfikujGen(j % size, wyn4.wartoscOsobnika((i + poz2) % size));
+                j++;
             }
-            for (i = 0; i < size ; i ++)
-            {
-                if (wyn3.wartoscOsobnika((i + poz2) % size) == null) continue;
-                else {
-                    wyn2.modyfikujGen(j % size ,wyn3.wartoscOsobnika((i + poz2)% size));
-                    j++;
-                    }
+        }
+        j = poz2;
+        for (int i = 0; i < size; i++) {
+            if (wyn3.wartoscOsobnika((i + poz2) % size) == iOsobnik.pusto) {
+                continue;
+            } else {
+                wyn1.modyfikujGen(j % size, wyn3.wartoscOsobnika((i + poz2) % size));
+                j++;
             }
-
         }
         return new Para<iOsobnik, iOsobnik>(wyn1, wyn2);
     }
-
 }
