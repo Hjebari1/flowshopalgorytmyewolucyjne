058ecc8766842379c0d820d80a94feb826cd6c3b
bananow <bananow@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-13 14:13:18 +0000

    Dwa działające operatory.Problem z operatorem OX

--- trunk/Algorytmy ewolucyjne flow shop/src/flowshop/operatorPMX.java
+++ trunk/Algorytmy ewolucyjne flow shop/src/flowshop/operatorPMX.java
@@ -29,18 +29,13 @@ public class operatorPMX implements iOperatorKrzyżowania {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public Para<iOsobnik, iOsobnik> krzyzuj(iOsobnik o1, iOsobnik o2) throws CloneNotSupportedException, Exception { //domyślnie private, dla testów public
+    public Para<iOsobnik, iOsobnik> krzyzuj(iOsobnik o1, iOsobnik o2,int poz1,int poz2) throws CloneNotSupportedException, Exception { //domyślnie private, dla testów public
          if (o1.dlugoscGenomu() != o2.dlugoscGenomu()) throw new Exception("Nierówne genomy do krzyżowania!");
         int size = o1.dlugoscGenomu();
         if (size == 0) throw new Exception("Pusty genom do krzyżowania!");
         iOsobnik wyn1 = o1.makeCopy();
         iOsobnik wyn2 = o2.makeCopy();
-        Random ktorePola = new Random();
-        int poz1 = ktorePola.nextInt(size);
-        int poz2 = ktorePola.nextInt(size);
-        while (poz1 == poz2) {
-            poz2 = ktorePola.nextInt(size); //!! TODO:: to tak nie można
-        }
+
         if (poz1 > poz2) {
             int tmp = poz1;
             poz1 = poz2;
@@ -61,8 +56,8 @@ public class operatorPMX implements iOperatorKrzyżowania {
             pozZmian = wyn1.znajdzPozGenuPoza(poz1, poz2, zmGen);
             wyn1.modyfikujGen(pozZmian, wyn2.wartoscOsobnika(i));
             zmGen = wyn2.wartoscOsobnika(i);
-            pozZmian = wyn1.znajdzPozGenuPoza(poz1, poz2, zmGen);
-            wyn1.modyfikujGen(pozZmian, wyn1.wartoscOsobnika(i));
+            pozZmian = wyn2.znajdzPozGenuPoza(poz1, poz2, zmGen);
+            wyn2.modyfikujGen(pozZmian, wyn1.wartoscOsobnika(i));
         }
 
         return new Para<iOsobnik, iOsobnik>(wyn1, wyn2);
