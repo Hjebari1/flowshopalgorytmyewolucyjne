a14f78910680719d4e4e205d4afa1dab106b2bc2
bananow <bananow@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-13 21:17:05 +0000

    Test to działania operatora na populacji.

--- trunk/Algorytmy ewolucyjne flow shop/test/flowshop/operatorOXTest.java
+++ trunk/Algorytmy ewolucyjne flow shop/test/flowshop/operatorOXTest.java
@@ -48,9 +48,34 @@ public class operatorOXTest {
         operatorOX instance = new operatorOX(null);
         iOsobnik wyn1 = new osobnikFlowShop(9, genom3);
         iOsobnik wyn2 = new osobnikFlowShop(9, genom4);
-        Para expResult = new Para<iOsobnik, iOsobnik>(wyn1,wyn2);
+        Para expResult = new Para<iOsobnik, iOsobnik>(wyn1, wyn2);
         Para result = instance.krzyzuj(o1, o2, 2, 5);
         System.out.print(result);
         assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testWykonaj() {
+        System.out.println("wykonaj");
+        populacja testRodzice = new populacja();
+        int[] genom1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] genom2 = {3, 2, 6, 7, 1, 5, 9, 4, 8};
+        int[] genom3 = {4, 5, 6, 7, 1, 9, 8, 2, 3};
+        int[] genom4 = {7, 1, 3, 4, 5, 8, 9, 2, 6};
+        testRodzice.dodajOsobnika(new osobnikFlowShop(9, genom1));
+        testRodzice.dodajOsobnika(new osobnikFlowShop(9, genom2));
+        testRodzice.dodajOsobnika(new osobnikFlowShop(9, genom3));
+        testRodzice.dodajOsobnika(new osobnikFlowShop(9, genom4));
+        operatorOX instance = new operatorOX(testRodzice);
+        populacja dzieci = instance.wykonaj();
+        assertEquals(dzieci.rozmiarPopulacji(), 4);
+        iOsobnik testOs = null;
+        while(dzieci.rozmiarPopulacji() > 0) {
+            testOs = dzieci.usunOsobnika(0);
+            for (int j = 1; j <= 9; j++) {
+                if (testOs.znajdzPozGenu(0, 9, j) == 9) fail("Nie znaleziono prawidlowego genu");
+            }
+        }
+        assertEquals(dzieci.rozmiarPopulacji(), 0);
     }
 }
