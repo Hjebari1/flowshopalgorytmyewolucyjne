14ad0c2f0a59690d5a6032050ec05cbc8e67cd32
synciu <synciu@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-16 22:38:08 +0000

    połączenie operatorów w jeden (klasa multiOperator) i  nowy Algorytm

---
+++ Algorytmy ewolucyjne flow shop/src/flowshop/Algorytm2.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flowshop;
+
+import flowshop.Interfejsy.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ListIterator;
+import java.util.Random;
+
+/**
+ *
+ * @author Łukasz Synówka
+ */
+public class Algorytm2 implements iAlgorytm
+{
+
+    populacja p;
+    populacja ps;
+    populacja pc;
+    iDane dane;
+    selekcjaRuletka sr;
+    iOperatorKrzyżowania oper;
+    zastepowanieTurniej zt;
+    int iloscOsobnikow;
+    funkcjaCeluFlowShop f;
+    iMutacja m;
+
+    public Algorytm2(int iloscOsobnikow,iDane d) throws FileNotFoundException
+    {
+        dane = d;
+        f = new funkcjaCeluFlowShop();
+        this.iloscOsobnikow=iloscOsobnikow;
+        p = new populacja();
+        zt = new zastepowanieTurniej(dane,f,iloscOsobnikow);
+        sr = new selekcjaRuletka(dane,iloscOsobnikow/2);
+        for(int i=0;i<iloscOsobnikow;i++)
+        {
+            p.dodajOsobnika(new osobnikFlowShop(d.iloscZadan()));
+        }
+        m= new Mutacja1();
+    }
+
+
+    public void wybor()
+    {
+        /*List<iOsobnik> li = new ArrayList();
+        li = sr.wybranaPopulacja(p,this.iloscOsobnikow/2);
+        while(!li.isEmpty())
+        {
+            pc.dodajOsobnika(li.remove(0));
+        }*/
+
+    }
+
+    public void krzyzowanie()
+    {
+        Random r = new Random();
+        oper = new multiOperator(p);
+        pc = oper.wykonaj();
+        if (r.nextInt(2)==0) m.wynonaj(pc);
+        if (r.nextInt(2)==0) m.wynonaj(p);
+
+    }
+
+    public void zastepowanie()
+    {
+        p=zt.wykonaj(p, pc);
+    }
+
+    @Override
+    public String toString()
+    {
+        String wynik = "";
+        ListIterator<iOsobnik> iter = p.popIterator();
+
+        double min=Double.MAX_VALUE;
+
+        osobnikFlowShop o;
+        while(iter.hasNext())
+        {
+            o=(osobnikFlowShop) iter.next();
+            min=Math.min(f.wartoscFunkcji(o, dane),min);
+            //wynik=wynik.concat(f.wartoscFunkcji(o, dane)+" ");
+        }
+        wynik=wynik.concat(min+"\n");
+
+        return wynik;
+
+    }
+
+
+}
---
+++ Algorytmy ewolucyjne flow shop/src/flowshop/multiOperator.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flowshop;
+
+import flowshop.Interfejsy.iOperatorKrzyżowania;
+import flowshop.Interfejsy.iOsobnik;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Łukasz Synówka
+ */
+public class multiOperator extends iOperatorKrzyżowania
+{
+    operatorCX cx;
+    operatorOX ox;
+    operatorPMX pmx;
+    populacja po;
+    public multiOperator(populacja p)
+    {
+        po=p;
+        cx= new operatorCX(p);
+        ox= new operatorOX(p);
+        pmx= new operatorPMX(p);
+    }
+
+    @Override
+    public populacja wykonaj()
+    {
+        Random losPoz = new Random();
+        populacja pochodneOsobniki = new populacja();
+        populacja rodzice = new populacja();
+        rodzice.polaczPopulacje(po);
+
+        while (rodzice.rozmiarPopulacji() > 0) {
+            try {
+                iOsobnik o1 = rodzice.usunOsobnika(losPoz.nextInt(rodzice.rozmiarPopulacji()));
+                iOsobnik o2 = rodzice.usunOsobnika(losPoz.nextInt(rodzice.rozmiarPopulacji()));
+                Object wynik = krzyzuj(o1, o2);
+                if ( wynik instanceof Para)
+                {
+
+                    pochodneOsobniki.dodajOsobnika((iOsobnik)((Para)wynik).getFirst());
+                    pochodneOsobniki.dodajOsobnika((iOsobnik)((Para)wynik).getSecond());
+                }
+                else
+                    pochodneOsobniki.dodajOsobnika((iOsobnik)wynik);
+            } catch (CloneNotSupportedException ex)
+            {
+                Logger.getLogger(operatorOX.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(operatorOX.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return pochodneOsobniki;
+
+
+}
+
+    private Object krzyzuj(iOsobnik o1, iOsobnik o2) throws CloneNotSupportedException, Exception
+    {
+        Random r = new Random();
+        int a=r.nextInt(3);
+        if (a==0)
+            return cx.krzyzuj(o1, o2);
+        if (a==1)
+        {
+            int poz1 = r.nextInt(o1.dlugoscGenomu());
+            int poz2 = r.nextInt(o2.dlugoscGenomu());
+            if (poz1 > poz2)
+            {
+                int tmp = poz1;
+                poz1 = poz2;
+                poz2 = tmp;
+            }
+            return ox.krzyzuj(o1, o2,poz1,poz2);
+        }
+        int poz1 = r.nextInt(o1.dlugoscGenomu());
+        int poz2 = r.nextInt(o2.dlugoscGenomu());
+        if (poz1 > poz2)
+        {
+            int tmp = poz1;
+            poz1 = poz2;
+            poz2 = tmp;
+        }
+        return ox.krzyzuj(o1, o2,poz1,poz2);
+
+    }
+}
