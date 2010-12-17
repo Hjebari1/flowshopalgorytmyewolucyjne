5af7a50f09510d8e305727f432f7f62c203fb747
bananow <bananow@df69c5ce-df32-f4cf-8dd3-398a37ccb6fc>
2010-12-17 11:39:32 +0000

    Działająca wersja selekcji

--- trunk/Algorytmy ewolucyjne flow shop/src/flowshop/selekcjaRuletka.java
+++ trunk/Algorytmy ewolucyjne flow shop/src/flowshop/selekcjaRuletka.java
@@ -5,9 +5,6 @@ import flowshop.Interfejsy.iFunkcjaCelu;
 import flowshop.Interfejsy.iOsobnik;
 import flowshop.Interfejsy.iSelekcja;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,6 +22,7 @@ import java.util.Random;
 public class selekcjaRuletka implements iSelekcja {
 
     iDane dane;
+
     public selekcjaRuletka(iDane dane) {
         this.dane = dane;
     }
@@ -36,62 +34,31 @@ public class selekcjaRuletka implements iSelekcja {
     public populacja wybranaPopulacja(populacja p, int rozmiar) {
         populacja wybrPop = new populacja();
         Random los = new Random();
-        HashMap<Double, List> wspTab = wyliczWsp(p);
-        ArrayList klucze = new ArrayList(wspTab.keySet());
-        Collections.sort(klucze,new Comparator<Double>() {
+        List<Para<Double, iOsobnik>> wspTab = wyliczWsp(p);
 
-            public int compare(Double o1, Double o2) {
-               if (o1 > o2) return -1;
-               else
-                   if (o1 < o2) return 1;
-                   else
-                       return 0;
-            }
-        });
         double wspPr;
         int odpSize = 0;
         double sum = 0;
         double prwd;
+        Para<Double,iOsobnik> tmp = null;
         while (odpSize < rozmiar) {
-            //sum = 0; //!! Linijka kołowości
-            prwd = los.nextDouble(); // to tutaj , czy przed while'm ?
-            for (Iterator kluczIter = klucze.iterator(); kluczIter.hasNext();) {
-                if (odpSize >= rozmiar) {
-                    break;
-                }
-                wspPr = (Double) kluczIter.next();
-                List wspList = wspTab.get(wspPr);
-                if (wspPr * wspList.size() + sum > prwd) {
-                    int poz = (int) Math.round(Math.ceil((prwd - sum) / wspPr)); //long to int!!
-                    odpSize++;
-                    wybrPop.dodajOsobnika((iOsobnik) wspList.get(poz-1));
-                    while (wspPr * (wspList.size() - poz) > prwd) { // ?
-                        if (odpSize >= rozmiar) {
-                            break;
-                        }
-                        poz += (int) Math.round(Math.ceil((prwd / wspPr))); //long to int!!
-                        if (poz <= wspList.size()) {
-                            odpSize++;
-                            wybrPop.dodajOsobnika((iOsobnik) wspList.get(poz-1));
-                        }
-                    }
-                    sum = (wspList.size() > poz ) ? ((wspList.size() - poz)*wspPr) : 0;
-                } else {
-                    sum += wspPr * wspList.size();
-                }
+            sum = 0;
+            prwd = los.nextDouble();
+            for (Iterator<Para<Double,iOsobnik>> i = wspTab.iterator(); i.hasNext() && sum < prwd;) {
+                tmp = i.next();
+                sum += tmp.getFirst();
             }
+            wybrPop.dodajOsobnika(tmp.getSecond());
         }
         return wybrPop;
     }
 
     /**
      * Funkcja prywatna, która wylicza współczynniki przystosownia osobników
-     * Współczynnik jest kluczem do listy iOsobników, których przystosowanie odpowiada kluczowi.
-     * Funkcja może być przekokszona (nadmiar użytych struktur).
      * @param daneWejsciowe populacja wejsciowa
-     * @return współczynniki przystosowania jako klucze wraz z odpowiednio przyporządkowanymi im iOsobnikami
+     * @return Para współczynnik, osobnik
      */
-    protected HashMap wyliczWsp(populacja daneWejsciowe) { // docelowo private, do testów protected
+    protected List<Para<Double, iOsobnik>> wyliczWsp(populacja daneWejsciowe) { // docelowo private, do testów protected
         iFunkcjaCelu f = new funkcjaCeluFlowShop(); //funkcja celu nie powinna być parametrem ?
         LinkedList<Double> wartosciOsobnikow = new LinkedList<Double>();
         double min = Double.MAX_VALUE, tmp, sum = 0;
@@ -107,21 +74,15 @@ public class selekcjaRuletka implements iSelekcja {
         // noralizacja współczynnika
         sum -= min * daneWejsciowe.rozmiarPopulacji();
 
-        HashMap<Double, List> wsp = new HashMap<Double, List>();
+        ArrayList<Para<Double, iOsobnik>> wsp = new ArrayList<Para<Double, iOsobnik>>();
+
         double wspPr;
         // iterator przewijający osobniki równolegle do przewijanych współczynników
         Iterator popIter = daneWejsciowe.popIterator();
 
         for (Iterator wartIter = wartosciOsobnikow.iterator(); wartIter.hasNext();) {
             wspPr = ((Double) wartIter.next() - min) / sum;
-            // sprawdzenie, czy lista na osobniki istnieje, jeśli nie tworzę nową
-            if (wsp.get(wspPr) != null) { //wyjatek dla listy pustej?
-                wsp.get(wspPr).add(popIter.next());
-            } else {
-                List list = new ArrayList<iOsobnik>();
-                list.add(popIter.next());
-                wsp.put(wspPr, list);
-            }
+            wsp.add(new Para(wspPr, popIter.next()));
         }
         return wsp;
     }
